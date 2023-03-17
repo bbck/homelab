d68e8ee37269c1c8d65917f6484f71d7252f1d84
Chris Boyle <chris@bbck.net>
2023-03-16 22:08:20 -0700

    Format js files

--- terraform/modules/lookitupongoogle/worker.js
+++ terraform/modules/lookitupongoogle/worker.js
@@ -1,14 +1,14 @@
 export default {
-  async fetch(request) {
-    const base = 'https://www.google.com';
-    const statusCode = 301;
+  async fetch (request) {
+    const base = 'https://www.google.com'
+    const statusCode = 301
 
-    const url = new URL(request.url);
-    const { pathname, search } = url;
+    const url = new URL(request.url)
+    const { pathname, search } = url
 
-    const destinationURL = `${base}${pathname}${search}`;
+    const destinationURL = `${base}${pathname}${search}`
     console.log(destinationURL)
 
-    return Response.redirect(destinationURL, statusCode);
-  },
-};
+    return Response.redirect(destinationURL, statusCode)
+  }
+}
