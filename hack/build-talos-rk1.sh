67f2f9c29d98059cbbfb48f252d6c1d98bfcbc58
Chris Boyle <chris@bbck.net>
2024-11-10 18:47:33 -0800

    Upgrade talos to v1.8.2

--- hack/build-talos-rk1.sh
+++ hack/build-talos-rk1.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -exo pipefail
 
-TALOS_VERSION="v1.7.6"
+TALOS_VERSION="v1.8.2"
 TALOS_EXTENSIONS="tailscale iscsi-tools util-linux-tools"
 
-WORK_DIR=$(mktemp -d)
-cleanup() { rm -rf "$WORK_DIR"; }
+WORK_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+cleanup() { rm -rf "$WORK_DIR"/_out; }
 trap cleanup EXIT
 
 _get_extension_images() {
-  crane export ghcr.io/siderolabs/extensions:$TALOS_VERSION | tar x -O image-digests | grep -E "$(echo $TALOS_EXTENSIONS | tr ' ' '|')"
+  crane export "ghcr.io/siderolabs/extensions:${TALOS_VERSION}" | tar x -O image-digests | grep -E "$(echo $TALOS_EXTENSIONS | tr ' ' '|')"
 }
 
 build_image() {
-  docker run --rm -t -v $PWD/_out:/out -v /dev:/dev --privileged ghcr.io/nberlee/imager:$TALOS_VERSION \
-    installer \
+  docker run --rm -t -v "${WORK_DIR}/_out:/out" -v /dev:/dev --privileged "ghcr.io/nberlee/imager:${TALOS_VERSION}" \
+    metal \
     --arch arm64 \
     --overlay-name turingrk1 \
     --overlay-image ghcr.io/nberlee/sbc-turingrk1:$TALOS_VERSION \
@@ -24,12 +24,12 @@ build_image() {
 }
 
 upload_image() {
-  gh auth token | crane auth login ghcr.io -u bbck --password-stdin
-  crane push _out/installer-arm64.tar ghcr.io/bbck/talos-rk1:$TALOS_VERSION
+  # https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-with-a-personal-access-token-classic
+  # gh auth token | crane auth login ghcr.io -u bbck --password-stdin
+  crane push "${WORK_DIR}/_out/installer-arm64.tar" "ghcr.io/bbck/talos-rk1:${TALOS_VERSION}"
 }
 
 main() {
-  cd "$WORK_DIR"
   build_image
   upload_image
 }
