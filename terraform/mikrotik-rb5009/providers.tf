c5d2c87ecc04e9789309d3f8322a85f77d83e147
Chris Boyle <154447+bbck@users.noreply.github.com>
2025-06-22 21:12:52 -0700

    Convert mikrotik terraform PoC to use 1password (#797)

--- terraform/mikrotik-rb5009/providers.tf
+++ terraform/mikrotik-rb5009/providers.tf
@@ -1,5 +1,10 @@
 terraform {
   required_providers {
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "2.1.2"
+    }
+
     routeros = {
       source  = "terraform-routeros/routeros"
       version = "1.85.0"
@@ -7,9 +12,23 @@ terraform {
   }
 }
 
+provider "onepassword" {
+  url   = var.op_connect_host
+  token = var.op_connect_token
+}
+
+data "onepassword_vault" "homelab" {
+  name = "Homelab"
+}
+
+data "onepassword_item" "routeros" {
+  vault = data.onepassword_vault.homelab.uuid
+  title = "routeros-terraform"
+}
+
 provider "routeros" {
-  hosturl  = "https://192.168.88.2"
+  hosturl  = var.routeros_host
   insecure = true
-  username = var.routeros_username
-  password = var.routeros_password
+  username = data.onepassword_item.routeros.username
+  password = data.onepassword_item.routeros.password
 }
